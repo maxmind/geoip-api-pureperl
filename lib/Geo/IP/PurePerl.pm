dee14983d9ffe2824f9fc825d0095a279b03cd43
Thomas J Mather <geoipapi@maxmind.com>
2005-05-27 15:46:04 +0000

    added support for GeoIP Netspeed

--- lib/Geo/IP/PurePerl.pm
+++ lib/Geo/IP/PurePerl.pm
@@ -17,6 +17,7 @@ use constant GEOIP_CITY_EDITION_REV0 => 111;
 use constant GEOIP_CITY_EDITION_REV1 => 2;
 use constant GEOIP_ORG_EDITION => 110;
 use constant GEOIP_ISP_EDITION => 109;
+use constant GEOIP_NETSPEED_EDITION => 10;
 use constant SEGMENT_RECORD_LENGTH => 3;
 use constant STANDARD_RECORD_LENGTH => 3;
 use constant ORG_RECORD_LENGTH => 4;
@@ -27,7 +28,6 @@ use constant CANADA_OFFSET => 677;
 use constant WORLD_OFFSET => 1353;
 use constant FIPS_RANGE => 360;
 
-
 $VERSION = '1.15';
 
 require Exporter;
@@ -36,8 +36,13 @@ require Exporter;
 sub GEOIP_STANDARD(){0;}
 sub GEOIP_MEMORY_CACHE(){1;}
 
-@EXPORT = qw( GEOIP_STANDARD GEOIP_MEMORY_CACHE );
+sub GEOIP_UNKNOWN_SPEED(){0;}
+sub GEOIP_DIALUP_SPEED(){1;}
+sub GEOIP_CABLEDSL_SPEED(){2;}
+sub GEOIP_CORPORATE_SPEED(){3;}
 
+@EXPORT = qw( GEOIP_STANDARD GEOIP_MEMORY_CACHE
+	      GEOIP_UNKNOWN_SPEED GEOIP_DIALUP_SPEED GEOIP_CABLEDSL_SPEED GEOIP_CORPORATE_SPEED );
 my @countries = (undef,"AP","EU","AD","AE","AF","AG","AI","AL","AM","AN","AO","AQ","AR","AS","AT","AU","AW","AZ","BA","BB","BD","BE","BF","BG","BH","BI","BJ","BM","BN","BO","BR","BS","BT","BV","BW","BY","BZ","CA","CC","CD","CF","CG","CH","CI","CK","CL","CM","CN","CO","CR","CU","CV","CX","CY","CZ","DE","DJ","DK","DM","DO","DZ","EC","EE","EG","EH","ER","ES","ET","FI","FJ","FK","FM","FO","FR","FX","GA","GB","GD","GE","GF","GH","GI","GL","GM","GN","GP","GQ","GR","GS","GT","GU","GW","GY","HK","HM","HN","HR","HT","HU","ID","IE","IL","IN","IO","IQ","IR","IS","IT","JM","JO","JP","KE","KG","KH","KI","KM","KN","KP","KR","KW","KY","KZ","LA","LB","LC","LI","LK","LR","LS","LT","LU","LV","LY","MA","MC","MD","MG","MH","MK","ML","MM","MN","MO","MP","MQ","MR","MS","MT","MU","MV","MW","MX","MY","MZ","NA","NC","NE","NF","NG","NI","NL","NO","NP","NR","NU","NZ","OM","PA","PE","PF","PG","PH","PK","PL","PM","PN","PR","PS","PT","PW","PY","QA","RE","RO","RU","RW","SA","SB","SC","SD","SE","SG","SH","SI","SJ","SK","SL","SM","SN","SO","SR","ST","SV","SY","SZ","TC","TD","TF","TG","TH","TJ","TK","TM","TN","TO","TP","TR","TT","TV","TW","TZ","UA","UG","UM","US","UY","UZ","VA","VC","VE","VG","VI","VN","VU","WF","WS","YE","YT","YU","ZA","ZM","ZR","ZW","A1","A2");
 my @code3s = ( undef,"AP","EU","AND","ARE","AFG","ATG","AIA","ALB","ARM","ANT","AGO","AQ","ARG","ASM","AUT","AUS","ABW","AZE","BIH","BRB","BGD","BEL","BFA","BGR","BHR","BDI","BEN","BMU","BRN","BOL","BRA","BHS","BTN","BV","BWA","BLR","BLZ","CAN","CC","COD","CAF","COG","CHE","CIV","COK","CHL","CMR","CHN","COL","CRI","CUB","CPV","CX","CYP","CZE","DEU","DJI","DNK","DMA","DOM","DZA","ECU","EST","EGY","ESH","ERI","ESP","ETH","FIN","FJI","FLK","FSM","FRO","FRA","FX","GAB","GBR","GRD","GEO","GUF","GHA","GIB","GRL","GMB","GIN","GLP","GNQ","GRC","GS","GTM","GUM","GNB","GUY","HKG","HM","HND","HRV","HTI","HUN","IDN","IRL","ISR","IND","IO","IRQ","IRN","ISL","ITA","JAM","JOR","JPN","KEN","KGZ","KHM","KIR","COM","KNA","PRK","KOR","KWT","CYM","KAZ","LAO","LBN","LCA","LIE","LKA","LBR","LSO","LTU","LUX","LVA","LBY","MAR","MCO","MDA","MDG","MHL","MKD","MLI","MMR","MNG","MAC","MNP","MTQ","MRT","MSR","MLT","MUS","MDV","MWI","MEX","MYS","MOZ","NAM","NCL","NER","NFK","NGA","NIC","NLD","NOR","NPL","NRU","NIU","NZL","OMN","PAN","PER","PYF","PNG","PHL","PAK","POL","SPM","PCN","PRI","PSE","PRT","PLW","PRY","QAT","REU","ROM","RUS","RWA","SAU","SLB","SYC","SDN","SWE","SGP","SHN","SVN","SJM","SVK","SLE","SMR","SEN","SOM","SUR","STP","SLV","SYR","SWZ","TCA","TCD","TF","TGO","THA","TJK","TKL","TLS","TKM","TUN","TON","TUR","TTO","TUV","TWN","TZA","UKR","UGA","UM","USA","URY","UZB","VAT","VCT","VEN","VGB","VIR","VNM","VUT","WLF","WSM","YEM","YT","YUG","ZAF","ZMB","ZR","ZWE","A1","A2");
 my @names = (undef,"Asia/Pacific Region","Europe","Andorra","United Arab Emirates","Afghanistan","Antigua and Barbuda","Anguilla","Albania","Armenia","Netherlands Antilles","Angola","Antarctica","Argentina","American Samoa","Austria","Australia","Aruba","Azerbaijan","Bosnia and Herzegovina","Barbados","Bangladesh","Belgium","Burkina Faso","Bulgaria","Bahrain","Burundi","Benin","Bermuda","Brunei Darussalam","Bolivia","Brazil","Bahamas","Bhutan","Bouvet Island","Botswana","Belarus","Belize","Canada","Cocos (Keeling) Islands","Congo, The Democratic Republic of the","Central African Republic","Congo","Switzerland","Cote D'Ivoire","Cook Islands","Chile","Cameroon","China","Colombia","Costa Rica","Cuba","Cape Verde","Christmas Island","Cyprus","Czech Republic","Germany","Djibouti","Denmark","Dominica","Dominican Republic","Algeria","Ecuador","Estonia","Egypt","Western Sahara","Eritrea","Spain","Ethiopia","Finland","Fiji","Falkland Islands (Malvinas)","Micronesia, Federated States of","Faroe Islands","France","France, Metropolitan","Gabon","United Kingdom","Grenada","Georgia","French Guiana","Ghana","Gibraltar","Greenland","Gambia","Guinea","Guadeloupe","Equatorial Guinea","Greece","South Georgia and the South Sandwich Islands","Guatemala","Guam","Guinea-Bissau","Guyana","Hong Kong","Heard Island and McDonald Islands","Honduras","Croatia","Haiti","Hungary","Indonesia","Ireland","Israel","India","British Indian Ocean Territory","Iraq","Iran, Islamic Republic of","Iceland","Italy","Jamaica","Jordan","Japan","Kenya","Kyrgyzstan","Cambodia","Kiribati","Comoros","Saint Kitts and Nevis",
@@ -138,32 +143,12 @@ sub _setup_segments {
     }
   }
   #if database Type is GEOIP_COUNTY_EDITION then use database segment GEOIP_COUNTRY_BEGIN
-  if ($gi->{"databaseType"} == GEOIP_COUNTRY_EDITION) {
+  if ($gi->{"databaseType"} == GEOIP_COUNTRY_EDITION ||
+      $gi->{"databaseType"} == GEOIP_NETSPEED_EDITION) {
     $gi->{"databaseSegments"} = GEOIP_COUNTRY_BEGIN;
   }
   seek($gi->{fh},$filepos,0);
   return $gi;
-}
-
-#this function returns the country code of ip address
-sub country_code_by_addr {
-  my ($gi, $ip_address) = @_;
-  return unless $ip_address =~ m!^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$!;
-  return $countries[$gi->_seek_country(addr_to_num($ip_address)) - GEOIP_COUNTRY_BEGIN];
-}
-
-#this function returns the country code3 of ip address
-sub country_code3_by_addr {
-  my ($gi, $ip_address) = @_;
-  return unless $ip_address =~ m!^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$!;
-  return $code3s[$gi->_seek_country(addr_to_num($ip_address)) - GEOIP_COUNTRY_BEGIN];
-}
-
-#this function returns the name of ip address
-sub country_name_by_addr {
-  my ($gi, $ip_address) = @_;
-  return unless $ip_address =~ m!^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$!;
-  return $names[$gi->_seek_country(addr_to_num($ip_address)) - GEOIP_COUNTRY_BEGIN];
 }
 
 sub _seek_country {
@@ -203,28 +188,55 @@ sub _seek_country {
   print STDERR "Error Traversing Database for ipnum = $ipnum - Perhaps database is corrupt?";
 }
 
+#this function returns the country code of ip address
+sub country_code_by_addr {
+  my ($gi, $ip_address) = @_;
+  return unless $ip_address =~ m!^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$!;
+  return $countries[$gi->id_by_addr($ip_address)];
+}
+
+#this function returns the country code3 of ip address
+sub country_code3_by_addr {
+  my ($gi, $ip_address) = @_;
+  return unless $ip_address =~ m!^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$!;
+  return $code3s[$gi->id_by_addr($ip_address)];
+}
+
+#this function returns the name of ip address
+sub country_name_by_addr {
+  my ($gi, $ip_address) = @_;
+  return unless $ip_address =~ m!^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$!;
+  return $names[$gi->id_by_addr($ip_address)];
+}
+
+sub id_by_addr {
+  my ($gi, $ip_address) = @_;
+  return unless $ip_address =~ m!^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$!;
+  return $gi->_seek_country(addr_to_num($ip_address)) - GEOIP_COUNTRY_BEGIN;
+}
+
 #this function returns the country code of domain name
 sub country_code_by_name {
   my ($gi, $host) = @_;
-  my $country_id = $gi->country_id_by_name($host);
+  my $country_id = $gi->id_by_name($host);
   return $countries[$country_id];
 }
 
 #this function returns the country code3 of domain name
 sub country_code3_by_name {
   my ($gi, $host) = @_;
-  my $country_id = $gi->country_id_by_name($host);
+  my $country_id = $gi->id_by_name($host);
   return $code3s[$country_id];
 }
 
 #this function returns the country name of domain name
 sub country_name_by_name {
   my ($gi, $host) = @_;
-  my $country_id = $gi->country_id_by_name($host);
+  my $country_id = $gi->id_by_name($host);
   return $names[$country_id];
 }
 
-sub country_id_by_name {
+sub id_by_name {
   my ($gi, $host) = @_;
   my $ip_address;
   if ($host =~ m!^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$!) {
@@ -599,7 +611,7 @@ http://sourceforge.net/projects/geoip/
 
 =head1 AUTHOR
 
-Copyright (c) 2004 MaxMind LLC
+Copyright (c) 2005 MaxMind LLC
 
 All rights reserved.  This package is free software; it is licensed
 under the GPL.
